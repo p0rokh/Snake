1530819b4cbc803386a276dd28c648aeffbc3c7d
p0rokh <anton.corolev@academmedia.com>
2016-03-17 09:38:43 +0600

    Create Project

---
+++ SnakeProject/SnakeProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SnakeProject
+//
+//  Created by Anton Korolev on 17.03.16.
+//  Copyright © 2016 Anton Korolev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
